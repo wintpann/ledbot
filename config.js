0e39739c65514c95cf612531cdfa2a108efc3479
Sergey Manshin <vvintpann@gmail.com>
2020-09-28 19:24:43 +0300

    disco best

--- config.js
+++ config.js
@@ -8,10 +8,8 @@ module.exports = {
     { brightness: 0.5, color: '#03d7ff' },
     { brightness: 1, color: '#ffee70' },
     { brightness: 0.5, color: '#49ff79' },
-    { brightness: 1, color: '#ffe1e4' },
-    { brightness: 0.5, color: '#ad62ff' },
-    { brightness: 1, color: '#ccff11' },
-    { brightness: 0.5, color: '#ff857b' },
     { brightness: 1, color: '#ff9ece' },
+    { brightness: 0.5, color: '#ad62ff' },
+    { brightness: 1, color: '#ff857b' },
   ]
 };
